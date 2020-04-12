8230100756c10ebb5af78d756a39088cd21be8e5
Emanuel Pécora <emanuelmza@gmail.com>
2020-04-12 20:03:44 -0300

    add tailwind.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import './styles/tailwind.generated.css';
 
 ReactDOM.render(
   <React.StrictMode>
