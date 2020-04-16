9270e9c0b432eaf3f70eb40c0c8cbcabaa61c85b
Emanuel Pécora <emanuelmza@gmail.com>
2020-04-16 10:41:34 -0300

    update text title.

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <h1 className="text-center text-xl md:text-4xl px-6 py-12 bg-white">
-      Our Products
+      Latest Cities
     </h1>
   );
 };
