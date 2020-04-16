ab1fe2d20c688f37189d71eb5614446db9edc64b
Emanuel Pécora <emanuelmza@gmail.com>
2020-04-16 10:41:06 -0300

    update sign in button.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ const Header = () => {
           href="#"
           className="block py-6 w-full text-center md:text-left md:w-auto text-gray-600 no-underline flex justify-center items-center"
         >
-          Your Logo
+          Weather App
         </a>
         <div className="w-full md:w-auto mb-6 md:mb-0 text-center md:text-right">
           <a
             href="#"
             className="inline-block no-underline bg-black text-white text-sm py-2 px-3"
           >
-            Login / Regsiter
+            Sign In / Sign Up
           </a>
         </div>
       </div>
